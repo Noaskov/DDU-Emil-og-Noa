8b4abbaec038e3f7c065fd993924343671292f74
Noaskov <n.c.skov@gmail.com>
2023-11-03 09:22:36 +0100

    move script work

--- DDU projekt 2 Emil og Noa 2023/Assets/Scripts/MovementScript.cs
+++ DDU projekt 2 Emil og Noa 2023/Assets/Scripts/MovementScript.cs
@@ -28,36 +28,40 @@ public class MovementScript : MonoBehaviour
         {
 
         isgrounded = Physics2D.Raycast(transform.position, Vector2.down, playerHeight * 0.5f + 0.3f, WhatIsGround);
-          //  rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
-          if(rb.velocity.x > maxSpeed)
-        {
-            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
-        } else if (rb.velocity.x < -maxSpeed)
-        {
-            rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
-        }
-        
-        
-        bool moveA = Input.GetKey(KeyCode.A);
-        bool moveD = Input.GetKey(KeyCode.D);
-        if(moveA && !moveD)
-        {
-            
-            rb.AddForce(new Vector2(-moveSpeed,0), ForceMode2D.Force);
-        } else if ( moveD && !moveA)
-        {
-            
-            rb.AddForce(new Vector2(moveSpeed, 0), ForceMode2D.Force);
-        }
+            //  rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+            /*
+            if(rb.velocity.x > maxSpeed)
+          {
+              rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+          } else if (rb.velocity.x < -maxSpeed)
+          {
+              rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+          }
 
-        if(Input.GetKeyDown(KeyCode.W) && isgrounded && canJump)
+
+          bool moveA = Input.GetKey(KeyCode.A);
+          bool moveD = Input.GetKey(KeyCode.D);
+          if(moveA && !moveD)
+          {
+
+              rb.AddForce(new Vector2(-moveSpeed,0), ForceMode2D.Force);
+          } else if ( moveD && !moveA)
+          {
+
+              rb.AddForce(new Vector2(moveSpeed, 0), ForceMode2D.Force);
+          }
+            */
+
+            Vector3 input = new Vector3( Input.GetAxisRaw("Horizontal"), 0, 0);
+            transform.position += input.normalized * moveSpeed * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.W) && isgrounded && canJump)
         {
             canJump = false;
             rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
             Invoke(nameof(ResetJump), 0.3f);
              
         }
-
+        
 
         }
     }
